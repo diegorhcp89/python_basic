a22a664622d2c1b42d24823d6f848af07d697ff2
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-09-10 12:07:42 -0300

    Adicianando aulas 1 a 10

---
+++ aula3.py
@@ -0,0 +1,18 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+# Aspas simples
+print('Diego Amaral')
+
+# Aspas duplas
+print("Diego Amaral")
+
+# Escape
+print("Diego \"Amaral\"")
+
+# r
+print(r"Diego \"Amaral\"")
