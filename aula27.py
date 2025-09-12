1362adf36018deffe31b458a7c0c7363f239eddd
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-09-12 17:57:30 -0300

    Aulas 21 a 30

---
+++ aula27.py
@@ -0,0 +1,12 @@
+"""
+Fatiamento de strings
+012345678
+olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd
+de caracteres da str
+"""
+variavel = 'Olá mundo'
+print(len(variavel))
+print(variavel[::-1])
