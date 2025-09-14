900515c74ca8fe79123af6f7e4ac47eb558cbcad
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-09-14 14:31:38 -0300

    Adicionando aulas 31 a 40

---
+++ aula39.py
@@ -0,0 +1,15 @@
+"""
+Interando strings com while
+"""
+#      012345678910
+nome = 'Luiz Otávio' # Iteráveis
+#      1110987654321
+
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += letra
+    indice += 1
+
+print(novo_nome)
